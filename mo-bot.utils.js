f7e9a02931c44565494c8dc547a4a331a9ff27ad
MWisBest <repinski23@gmail.com>
2012-02-19 17:32:10 -0600

    Added - jQuery's AJAX be able to possibly use GML_xmlhttpRequest -- Would allow it to access non-same-origin pages
    -- No version bump.

--- mo-bot.utils.js
+++ mo-bot.utils.js
@@ -101,3 +101,84 @@ function generateOvertake()
 	str = str.substring( randomNumber, randomNumber + 5 );
 	return str;
 }
+
+function GM_XHR() // Going to replace getPage with this so i can use jQuery's goodies of AJAX
+{
+	this.type = null;
+	this.url = null;
+	this.async = null;
+	this.username = null;
+	this.password = null;
+	this.status = null;
+	this.headers = {};
+	this.readyState = null;
+
+	this.abort = function()
+	{
+		this.readyState = 0;
+	};
+
+	this.getAllResponseHeaders = function( name )
+	{
+	  if( this.readyState != 4 ) return "";
+	  return this.responseHeaders;
+	};
+
+	this.getResponseHeader = function( name )
+	{
+	  var regexp = new RegExp( "^" + name + ": (.*)$", "im" );
+	  var match = regexp.exec( this.responseHeaders );
+	  if( match ) { return match[1]; }
+	  return '';
+	};
+
+	this.open = function( type, url, async, username, password )
+	{
+		this.type = type ? type : null;
+		this.url = url ? url : null;
+		this.async = async ? async : null;
+		this.username = username ? username : null;
+		this.password = password ? password : null;
+		this.readyState = 1;
+	};
+	
+	this.setRequestHeader = function( name, value )
+	{
+		this.headers[name] = value;
+	};
+
+	this.send = function( data )
+	{
+		this.data = data;
+		var that = this;
+		GM_xmlhttpRequest(
+		{
+			method: this.type,
+			url: this.url,
+			headers: this.headers,
+			data: this.data,
+			onload: function( rsp )
+			{
+				for( k in rsp )
+				{
+					that[k] = rsp[k];
+				}
+				that.onreadystatechange();
+			},
+			onerror: function( rsp )
+			{
+				for( k in rsp )
+				{
+					that[k] = rsp[k];
+				}
+			}
+		}
+		);
+	};
+};
+
+jQuery.ajaxSetup(
+{
+    xhr: function() { return new GM_XHR; }
+}
+);
