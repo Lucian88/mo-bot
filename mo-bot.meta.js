19fadb949859b04bf2342bf771aee69cffe5354b
MwisBest <repinski23@gmail.com>
2013-04-01 18:21:57 -0500

    I suppose the meta needs to be updated as well.

--- mo-bot.meta.js
+++ mo-bot.meta.js
@@ -6,11 +6,11 @@
 // @contributor  Mobster (244080236)
 // @description  JavaScript bot for Xat Mobile.
 // @include      http://m.xat.com:10049/*
-// @version      0.6.0.0
+// @version      0.6.8.0
 // @icon         https://mo-bot.googlecode.com/hg/icons/Mo-Bot.png
 // @icon64       https://mo-bot.googlecode.com/hg/icons/Mo-Bot.png
 // @homepage     http://code.google.com/p/mo-bot/
-// @require      https://mo-bot.googlecode.com/hg/jquery-1.7.1.js
-// @require      https://mo-bot.googlecode.com/hg/mo-bot.utils-1.0.1.js
+// @require      https://mo-bot.googlecode.com/hg/jquery-1.9.1.js
+// @require      https://mo-bot.googlecode.com/hg/mo-bot.utils-1.1.0.js
 // @updateURL    https://mo-bot.googlecode.com/hg/mo-bot.meta.js
 // ==/UserScript==
